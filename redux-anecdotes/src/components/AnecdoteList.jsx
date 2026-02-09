6caa3d0b3c7326155dc622a85d285f568dce5a12
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2026-02-09 13:50:43 +0200

    Tehtävä 6.9: Filtteröinti toteutettu Reduxilla

--- redux-anecdotes/src/components/AnecdoteList.jsx
+++ redux-anecdotes/src/components/AnecdoteList.jsx
@@ -4,9 +4,11 @@ import { voteAnecdote } from '../reducers/anecdoteReducer'
 const AnecdoteList = () => {
   const dispatch = useDispatch()
 
-  const anecdotes = useSelector(state =>
-    [...state].sort((a, b) => b.votes - a.votes)
-  )
+  const anecdotes = useSelector(({ anecdotes, filter }) => {
+    return anecdotes
+      .filter(a => a.content.toLowerCase().includes(filter.toLowerCase()))
+      .sort((a, b) => b.votes - a.votes)
+  })
 
   return (
     <div>
