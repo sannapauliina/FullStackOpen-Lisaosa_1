f829b204390b65e01243c2f74fe7f080398908dd
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2026-02-09 13:33:34 +0200

    Tehtävä 6.8: AnecdoteList‑komponentti luotu ja lista eriytetty

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -1,31 +1,15 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { voteAnecdote } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const dispatch = useDispatch()
-  const anecdotes = useSelector(state =>
-    [...state].sort((a, b) => b.votes - a.votes)
-  )
-
   return (
     <div>
-      <AnecdoteForm />
-
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => dispatch(voteAnecdote(anecdote.id))}>
-              vote
-            </button>
-          </div>
-        </div>
-      )}
+      <AnecdoteForm />
+      <AnecdoteList />
     </div>
   )
 }
 
 export default App
+
---
+++ redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,28 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { voteAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+  const dispatch = useDispatch()
+
+  const anecdotes = useSelector(state =>
+    [...state].sort((a, b) => b.votes - a.votes)
+  )
+
+  return (
+    <div>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => dispatch(voteAnecdote(anecdote.id))}>
+              vote
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default AnecdoteList
