9cc1153404fe7eddc5834efad3aba447219b4b99
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2026-02-09 12:56:32 +0200

    Tehtävä 6.3: Anekdoottien äänestys toteutettu Reduxilla

---
+++ redux-anecdotes/src/App.jsx
@@ -0,0 +1,28 @@
+import { useSelector, useDispatch } from 'react-redux'
+import { voteAnecdote } from './reducers/anecdoteReducer'
+
+const App = () => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state)
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => dispatch(voteAnecdote(anecdote.id))}>
+              vote
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
