932c1260945cfaaa59991001a72dd814aa351adf
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2026-02-09 14:05:17 +0200

    Tehtävä 6.10: Redux Toolkit otettu käyttöön, filterSlice luotu ja DevTools käytössä

--- redux-anecdotes/src/reducers/filterReducer.js
+++ redux-anecdotes/src/reducers/filterReducer.js
@@ -1,17 +1,15 @@
-const filterReducer = (state = '', action) => {
-  switch (action.type) {
-    case 'SET_FILTER':
+import { createSlice } from '@reduxjs/toolkit'
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    setFilter(state, action) {
       return action.payload
-    default:
-      return state
+    }
   }
-}
+})
 
-export const setFilter = (value) => {
-  return {
-    type: 'SET_FILTER',
-    payload: value
-  }
-}
+export const { setFilter } = filterSlice.actions
+export default filterSlice.reducer
 
-export default filterReducer
--- redux-anecdotes/src/store.js
+++ redux-anecdotes/src/store.js
@@ -1,12 +1,12 @@
-import { createStore, combineReducers } from 'redux'
+import { configureStore } from '@reduxjs/toolkit'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  filter: filterReducer
+const store = configureStore({
+  reducer: {
+    anecdotes: anecdoteReducer,
+    filter: filterReducer
+  }
 })
-
-const store = createStore(reducer)
 
 export default store
